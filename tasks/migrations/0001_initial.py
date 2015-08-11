df6a0da974f802ac750ce743bf2f46ccf90718f3
Kriti Godey <kriti@kritigodey.com>
2015-08-11 16:48:02 -0400

    make sure list name is unique

--- tasks/migrations/0001_initial.py
+++ tasks/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             name='List',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=255)),
+                ('name', models.CharField(unique=True, max_length=255)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
                 ('archived', models.BooleanField(default=False)),
--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class List(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     archived = models.BooleanField(default=False)
