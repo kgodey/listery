6fc3b0e979c54e48e04c663764b5f45f14904800
Kriti Godey <kriti@kritigodey.com>
2015-08-20 00:18:39 -0400

    Set up authentication, login and logout

--- tasks/apis.py
+++ tasks/apis.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from tasks.models import List, ListItem
@@ -12,19 +12,19 @@ from tasks.serializers import UserSerializer, ListSerializer, ListItemSerializer
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
 
 class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.filter(archived=False)
     serializer_class = ListSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
 
 class ListItemViewSet(viewsets.ModelViewSet):
     queryset = ListItem.objects.order_by('order')
     serializer_class = ListItemSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     
     @detail_route(methods=['post'])
     def reorder(self, request, pk=None):
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from tasks.models import List
 
 
+@login_required
 def index(request):
     lists = List.objects.all()
     return render(request, 'tasks/index.html', {'lists': lists})
