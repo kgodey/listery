37c01042fec4f0d0c6123ebf25c4344b350dee25
Kriti Godey <kriti@kritigodey.com>
2015-09-06 17:39:11 -0400

    provide a default is LISTERY_TITLE is not set

--- listery/context_processors.py
+++ listery/context_processors.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 
 def listery_info(request):
-	return {'LISTERY_TITLE': settings.LISTERY_TITLE }
+	return {'LISTERY_TITLE': settings.LISTERY_TITLE if hasattr (settings, 'LISTERY_TITLE') else 'Listery' }
