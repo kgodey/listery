d9aca7afcd51509678498aa8cc3f4800285985af
Kriti Godey <kriti@kritigodey.com>
2016-08-14 17:09:12 -0400

    Remove incorrect staticfiles dir

--- example_project/settings.py
+++ example_project/settings.py
@@ -139,6 +139,3 @@ if 'DATABASE_URL' in os.environ:
 	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 	STATIC_ROOT = 'staticfiles'
 
-	STATICFILES_DIRS = (
-	    os.path.join(BASE_DIR, 'static'),
-	)
