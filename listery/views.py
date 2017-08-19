38504e04518db112b6feff0e005e2b6b2b71ef96
Kriti Godey <kriti@kritigodey.com>
2017-08-18 23:36:25 -0400

    Consolidated common query parameters into querysets on the models

--- listery/views.py
+++ listery/views.py
@@ -13,6 +13,6 @@ def index(request):
 
 @login_required
 def new_index(request):
-	first_list = List.objects.filter(Q(owner=request.user) | Q(private=False)).order_by('id').first()
+	first_list = List.objects.all_for_user(request.user).first()
 	first_list_id = first_list.id if first_list else None
 	return render(request, 'listery/listery_v2/index.html', {'first_list_id': first_list_id})
