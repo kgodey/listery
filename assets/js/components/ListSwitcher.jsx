798d991966015480886815d93876ff3a866611dc
Kriti Godey <kriti@kritigodey.com>
2017-08-18 23:50:39 -0400

    API optimization for list actions & setting active list when rendering list switcher

--- assets/js/components/ListSwitcher.jsx
+++ assets/js/components/ListSwitcher.jsx
@@ -5,11 +5,12 @@ import { ListLink } from './ListLink.jsx'
 export class ListSwitcher extends React.Component {
 	render() {
 		const allLists = this.props.allLists ? this.props.allLists : []
+		const activeListId = this.props.activeListId
 		if (allLists.length > 0) {
 			return (
 				<div className='list-group'>
 					{allLists.map(list =>
-						<ListLink key={list.id} {...list}/>
+						<ListLink key={list.id} {...list} activeList={list.id == activeListId ? true : false}/>
 					)}
 				</div>
 			);
--- listery/apis/v2_apis.py
+++ listery/apis/v2_apis.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from listery.filters import ListFilter, ListItemFilter
 from listery.models import List, ListItem
-from listery.serializers.v2_serializers import ListSerializer, ListItemSerializer
+from listery.serializers.v2_serializers import ListSerializer, ListItemSerializer, MinimalListSerializer
 
 
 class ListViewSet(viewsets.ModelViewSet):
@@ -28,3 +28,8 @@ class ListItemViewSet(viewsets.ModelViewSet):
 
 	def get_queryset(self):
 		return self.queryset.all_for_user(self.request.user)
+
+	def get_serializer_class(self):
+		if self.action == 'list':
+			return MinimalListSerializer
+		return ListSerializer
