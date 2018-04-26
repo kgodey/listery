a7e35eb27e74c1d233ada5de1767d3cfb67035cd
Kriti Godey <kriti@kritigodey.com>
2018-04-26 00:29:03 -0400

    Allow editing of descriptions of list items.

--- assets/js/components/App/ActiveList/ListItem/Description.jsx
+++ assets/js/components/App/ActiveList/ListItem/Description.jsx
@@ -6,9 +6,30 @@ const descriptionStyle = {
 	paddingTop: 0
 }
 
+const descriptionEditStyle = {
+	width: '84%',
+	marginTop: '0.5em',
+	fontSize: '0.8em'
+}
+
 
 export const Description = (props) => {
-	return (
-		<small><i><span style={descriptionStyle}>{props.description}</span></i></small>
-	)
+	if (props.currentlyEditing) {
+		return (
+			<input
+				type='text'
+				maxLength='255'
+				placeholder='Description'
+				style={descriptionEditStyle}
+				value={props.description}
+				onChange={props.onChange}
+				onKeyUp={props.onKeyUp}
+				onBlur={props.onBlur}
+			/>
+		)
+	} else {
+		return (
+			<small><i><span style={descriptionStyle}>{props.description}</span></i></small>
+		)
+	}
 }
