5f13ef40a3915dc26a533bc9f1c3ecea008590b3
Kriti Godey <kriti@kritigodey.com>
2019-01-02 00:24:09 -0500

    Fixed pylint errors.

--- listery/admin.py
+++ listery/admin.py
@@ -8,9 +8,10 @@ from listery.models import List, ListItem
 
 
 class ListeryAdminSite(AdminSite):
+	"""Admin site class for Listery, nothing fancy."""
 	site_header = 'Listery administration'
 
 
-admin_site = ListeryAdminSite(name='listery')
+admin_site = ListeryAdminSite(name='listery') # pylint: disable=invalid-name
 admin_site.register(List)
 admin_site.register(ListItem)
--- listery/tests/test_apis.py
+++ listery/tests/test_apis.py
@@ -6,8 +6,6 @@ from rest_framework.reverse import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from listery.models import List
-
 
 class ListAPITests(APITestCase):
 	"""Tests for the List API"""
