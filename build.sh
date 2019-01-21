a0792317be70a8594ecdd547a5684ae879173d73
Kriti Godey <kriti@kritigodey.com>
2019-01-21 00:09:22 -0500

    Made directory before attempting to copy stuff to it.

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 webpack --mode=production --progress --colors -p
+mkdir -p listery/static/listery/css/bootstrap/
 cp -r node_modules/bootstrap/dist/css/bootstrap.min.css* listery/static/listery/css/bootstrap/
