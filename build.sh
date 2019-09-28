245b0dcf19bcb9170a15c8126511b0ed73508e91
Kriti Godey <kriti@kritigodey.com>
2019-09-28 02:32:57 -0400

    Installed Framework 7

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 webpack --mode=production --progress --colors -p
 mkdir -p listery/static/listery/css/bootstrap/
+mkdir -p listery/static/listery/css/framework7/
 cp -r node_modules/bootstrap/dist/css/bootstrap.min.css* listery/static/listery/css/bootstrap/
+cp -r node_modules/framework7/css/framework7.bundle.min.css* listery/static/listery/css/framework7/
 cp assets/css/style.css listery/static/listery/css/
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 webpack --mode=development --progress --colors --display-error-details -debug
 cp -r node_modules/bootstrap/dist/css/bootstrap.min.css* listery/static/listery/css/bootstrap/
+cp -r node_modules/framework7/css/framework7.bundle.min.css* listery/static/listery/css/framework7/
 cp assets/css/style.css listery/static/listery/css/
