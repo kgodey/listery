49142726ed6a296a5ec5c36874c4c8b8edf6c994
Kriti Godey <kriti@kritigodey.com>
2019-09-29 02:53:21 -0400

    Archiving lists works (but redirecting to the new list does not).

--- listery/assets/js/components/MobileApp/ListSwitcher/ListLink.jsx
+++ listery/assets/js/components/MobileApp/ListSwitcher/ListLink.jsx
@@ -1,34 +1,61 @@
-import { ListItem } from 'framework7-react'
+import { ListItem, SwipeoutActions, SwipeoutButton } from 'framework7-react'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { fetchActiveList } from '../../../actions/list'
+import { fetchActiveList, archiveList } from '../../../actions/list'
+import { getNextList } from '../../../reducers/allLists'
 import { getActiveListID } from '../../../reducers/activeList'
 
 
 class ListLink extends React.Component {
+	constructor(props) {
+		super(props)
+		this.handleArchived = this.handleArchived.bind(this)
+	}
+
+	handleArchived(event) {
+		const { archiveList, nextListID } = this.props
+		archiveList(nextListID)
+	}
+
 	render() {
 		const { id, name, onClick, activeListID } = this.props
 		return (
 			<ListItem
+				swipeout
+				onSwipeoutDeleted={this.handleArchived}
 				title={name}
 				bgColor={activeListID == id ? 'primary' : 'black'}
 				onClick={onClick}
-			/>
+			>
+				<SwipeoutActions right>
+					<SwipeoutButton
+						delete
+						confirmText="Are you sure you want to archive this list?"
+						confirmTitle="Confirm Archival"
+				>
+						Archive
+					</SwipeoutButton>
+				</SwipeoutActions>
+			</ListItem>
 		)
 	}
 }
 
 
 const mapStateToProps = (state, ownProps) => ({
-	activeListID: getActiveListID(state)
+	activeListID: getActiveListID(state),
+	nextListID: getNextList(state, ownProps.id)
 })
 
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
 	onClick: () => {
 		dispatch(fetchActiveList({id: ownProps.id}))
+	},
+	archiveList: (nextListID) => {
+		dispatch(archiveList(ownProps.id, nextListID))
 	}
 })
 
@@ -36,8 +63,10 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
 ListLink.propTypes = {
 	id: PropTypes.number.isRequired,
 	name: PropTypes.string.isRequired,
+	activeListID: PropTypes.number,
+	nextListID: PropTypes.number,
 	onClick: PropTypes.func.isRequired,
-	activeListID: PropTypes.number
+	archiveList: PropTypes.func.isRequired
 }
 
 ListLink = connect(mapStateToProps, mapDispatchToProps)(ListLink)
